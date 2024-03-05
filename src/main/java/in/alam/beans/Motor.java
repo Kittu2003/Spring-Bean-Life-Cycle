46835bb425f52cae4b7f7fb1d05a1ea732c091ae
Kittu2003 <khalidalam3254@gmail.com>
2024-03-05 20:55:22 +0530

    first commit

---
+++ src/main/java/in/alam/beans/Motor.java
@@ -0,0 +1,65 @@
+  //Bean Life Cycle using XML Approach
+
+
+/*package in.alam.beans;
+
+public class Motor {
+  
+	public Motor() {
+		System.out.println("Motor:: Constructor");
+	}
+	
+	public void start() {
+		System.out.println("Motor:: started" );
+	}
+	
+	public void dowork() {
+		System.out.println("Motor :: pulling the water ");
+	}
+	 
+	
+	public void stop() {
+		System.out.println("Motor stopped");
+	}
+}
+*/
+      
+
+     //Bean Life Cycle using Programmatic Approach
+
+package in.alam.beans;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Motor implements InitializingBean ,DisposableBean{
+  
+	public Motor() {
+		System.out.println("Motor:: Constructor");
+	}
+	
+	public void dowork() {
+		System.out.println("Motor :: pulling the water ");
+	}
+         //    3 Using Annotation Approach
+	     //@PostConstruct
+	     //public void m1() throws Exception {
+	     //System.out.println("Motor:: started" );
+	     //}
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Motor:: started" );
+		
+	}
+	
+          //   3 Using Annotation Approach
+          //@Predestroy
+          //public void m1() throws Exception {
+          //System.out.println("Motor:: started" );
+          //}
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("Motor stopped");
+		
+	}
+}
